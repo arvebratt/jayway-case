d4028758997949e6a10de47789e4301ecc4c75f4
Alexander Arvebratt <arvebratt@protonmail.com>
2021-04-12 12:06:39 +0200

    fixing favicon

--- components/Meta.js
+++ components/Meta.js
@@ -8,7 +8,7 @@ export default function Meta({ title, keywords, description }) {
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
-      <link rel="icon" href="../public/favicon.ico" />
+      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap"
